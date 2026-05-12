23a7bd4ce183614098c7c6b1cb1751a67f025499
kaua <kaua.santanaj@gmail.com>
2026-05-12 15:49:02 -0300

    adding factoryMethod implementations

---
+++ src/main/java/com/suportChat/project/model/factory/implementations/DefaultQueueTicketFactory.java
@@ -0,0 +1,44 @@
+package com.suportChat.project.model.factory.implementations;
+
+import com.suportChat.project.model.entity.QueueTicket;
+import com.suportChat.project.model.factory.contracts.QueueTicketFactory;
+import com.suportChat.project.model.roles.QueuTickerEnum;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class DefaultQueueTicketFactory implements QueueTicketFactory {
+
+
+    @Override
+    public void createTicket(String name) {
+        QueueTicket queueTicket = new QueueTicket();
+        queueTicket.setName(name);
+        queueTicket.setPriority(2);
+        queueTicket.setCode(generateCode());
+        queueTicket.setLocalDateTime(LocalDateTime.now());
+        queueTicket.setQueuTickerEnum(QueuTickerEnum.DEFAULT);
+
+    }
+
+
+    private String generateCode(){
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < 4; i++){
+            int index = random.nextInt(chars.length());
+            builder.append(chars.charAt(index));
+
+        }
+
+
+        return builder.toString();
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/suportChat/project/model/factory/implementations/PcdQueueTicketFactory.java
@@ -0,0 +1,34 @@
+package com.suportChat.project.model.factory.implementations;
+
+import com.suportChat.project.model.entity.QueueTicket;
+import com.suportChat.project.model.factory.contracts.QueueTicketFactory;
+import com.suportChat.project.model.roles.QueuTickerEnum;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class PcdQueueTicketFactory implements QueueTicketFactory {
+
+    @Override
+    public void createTicket(String name) {
+        QueueTicket queueTicket = new QueueTicket();
+        queueTicket.setName(name);
+        queueTicket.setPriority(0);
+        queueTicket.setCode(generateCode());
+        queueTicket.setLocalDateTime(LocalDateTime.now());
+        queueTicket.setQueuTickerEnum(QueuTickerEnum.PCD);
+    }
+
+    private String generateCode(){
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = 0; i < 4; i++){
+            int index = random.nextInt(chars.length());
+            builder.append(chars.charAt(index));
+        }
+        return builder.toString();
+
+    }
+}
