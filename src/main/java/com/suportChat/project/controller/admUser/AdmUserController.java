cb84edd9e73fa875a64664bd41651f9cfa4905a0
kaua <kaua.santanaj@gmail.com>
2026-05-13 16:58:24 -0300

    adding admUser controller

--- src/main/java/com/suportChat/project/controller/admUser/AdmUserController.java
+++ src/main/java/com/suportChat/project/controller/admUser/AdmUserController.java
@@ -1,7 +1,13 @@
 package com.suportChat.project.controller.admUser;
 
 
+import com.suportChat.project.dto.request.AdmUserLoginRequest;
+import com.suportChat.project.dto.request.AdmUserRegisterRequest;
+import com.suportChat.project.dto.response.AdmUserRegisterResponse;
+import com.suportChat.project.dto.response.AdmUserTokenResponse;
 import com.suportChat.project.service.admUser.AdmUserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +20,20 @@ public class AdmUserController {
     public AdmUserController(AdmUserService admUserService) {
         this.admUserService = admUserService;
     }
+
+    @PostMapping("/register")
+    public AdmUserTokenResponse login(@RequestBody AdmUserLoginRequest request){
+        return admUserService.login(request);
+    }
+
+    @PostMapping("/login")
+    public AdmUserRegisterResponse register(@RequestBody AdmUserRegisterRequest request){
+        return admUserService.register(request);
+    }
+
+
+
+
+
+
 }
